82b01e5b5845cb1324443b77f2c8c890ad8b4bc2
sabourisly <142976254+sabourisly@users.noreply.github.com>
2023-09-11 19:33:43 +0300

    saad

--- FINAL_PROJECT.py
+++ FINAL_PROJECT.py
@@ -1,21 +1,33 @@
 import pygame
 import time
 import random
+
 pygame.font.init()
 
 WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("survive the jungle")
 
-BG = pygame.transform.scale(pygame.image.load("survive the jungle-wallpaper-preview.jpg"), (WIDTH, HEIGHT))
+BG = pygame.transform.scale(
+    pygame.image.load("survive the jungle-wallpaper-preview.jpg"), (WIDTH, HEIGHT)
+)
+
+PREY_IMG = pygame.image.load("prey.survive_the_jungle.png")
+ANIMAL_IMG = pygame.image.load("predator.survive_the_jungle.png")
+
+
 
 PLAYER_WIDTH = 40
 PLAYER_HEIGHT = 60
 
 PLAYER_VEL = 5
-STAR_WIDTH = 10
-STAR_HEIGHT = 20
+STAR_WIDTH = 50
+STAR_HEIGHT = 50
 STAR_VEL = 3
+
+PREY_IMG = pygame.transform.scale(PREY_IMG, (PLAYER_WIDTH, PLAYER_HEIGHT))
+ANIMAL_IMG = pygame.transform.scale(ANIMAL_IMG, (STAR_WIDTH, STAR_HEIGHT))
+
 
 FONT = pygame.font.SysFont("comicsans", 30)
 
@@ -26,10 +38,10 @@ def draw(player, elapsed_time, stars):
     time_text = FONT.render(f"Time: {round(elapsed_time)}s", 1, "white")
     WIN.blit(time_text, (10, 10))
 
-    pygame.draw.rect(WIN, "white", player)
+    WIN.blit(PREY_IMG, (player.x, player.y))
 
     for star in stars:
-        pygame.draw.rect(WIN, "red", star)
+        WIN.blit(ANIMAL_IMG, (star.x, star.y))
 
     pygame.display.update()
 
@@ -37,8 +49,7 @@ def draw(player, elapsed_time, stars):
 def main():
     run = True
 
-    player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT,
-                         PLAYER_WIDTH, PLAYER_HEIGHT)
+    player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
     clock = pygame.time.Clock()
     start_time = time.time()
     elapsed_time = 0
@@ -56,8 +67,7 @@ def main():
         if star_count > star_add_increment:
             for _ in range(3):
                 star_x = random.randint(0, WIDTH - STAR_WIDTH)
-                star = pygame.Rect(star_x, -STAR_HEIGHT,
-                                   STAR_WIDTH, STAR_HEIGHT)
+                star = pygame.Rect(star_x, -STAR_HEIGHT, STAR_WIDTH, STAR_HEIGHT)
                 stars.append(star)
 
             star_add_increment = max(200, star_add_increment - 50)
@@ -85,7 +95,13 @@ def main():
 
         if hit:
             lost_text = FONT.render("You Lost!", 1, "white")
-            WIN.blit(lost_text, (WIDTH/2 - lost_text.get_width()/2, HEIGHT/2 - lost_text.get_height()/2))
+            WIN.blit(
+                lost_text,
+                (
+                    WIDTH / 2 - lost_text.get_width() / 2,
+                    HEIGHT / 2 - lost_text.get_height() / 2,
+                ),
+            )
             pygame.display.update()
             pygame.time.delay(4000)
             break
